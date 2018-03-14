c72f4a75186d9987441860a8f78158a9ad78feb0
Gian Carlo <giancarlo14.11@gmail.com>
2018-03-14 12:24:32 +0100

    12. Solucionado el bug con el comando print

--- Game.java
+++ Game.java
@@ -159,11 +159,8 @@ public class Game
      */
     private void printHelp() 
     {
-        System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
-        System.out.println();
         System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        parser.showCommands();
     }
 
     /** 
